4ae424420326f0c2df630e750cfe95412595bec5
mis <satoutahhaithem@gmail.com>
2024-02-13 16:33:16 +0100

    add matrix for affectation

--- main.py
+++ main.py
@@ -11,10 +11,23 @@ workingGroupNames= ["TADJ","DAAO","ROSA","ROET","ROES","DOR","META","GT2L","OR",
 "ROQ","ATOM","ROCT","GOTHA","POC","SCALE","COSMOS","OM"]
 print (workingGroupNames)
 
-SlotsparrallelSession = np.arange(1,8)
-print(SlotsparrallelSession)
+SlotsParrallelSession = np.arange(1,8)
+print(SlotsParrallelSession)
 
 possibleAmountOfPapers = np.arange(3,7)
 print(possibleAmountOfPapers)
 
+maxparallelSession = 11
 
+authorizedPapersForEachSlots = {
+    1:4,
+    2:6,
+    3:6,
+    4:4,
+    5:4,
+    6:5,
+    7:3
+}
+print (authorizedPapersForEachSlots)
+
+affectationMatrix = np.zeros((7,11))
