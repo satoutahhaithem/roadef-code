19cfc08a49f763691fca71f42a9a1c6f66b6f2b2
mis <satoutahhaithem@gmail.com>
2024-02-13 15:38:48 +0100

    defining variable

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello")
+import numpy as np
+
+
+conferenceSession = np.arange(1,41)
+print (conferenceSession)
+
+workingGroup = np.arange(1,21)
+print (workingGroup)
+
+workingGroupNames= ["TADJ","DAAO","ROSA","ROET","ROES","DOR","META","GT2L","OR","CAGDO","ORIGIN","P2LS",
+"ROQ","ATOM","ROCT","GOTHA","POC","SCALE","COSMOS","OM"]
+print (workingGroupNames)
+
+SlotsparrallelSession = np.arange(1,8)
+print(SlotsparrallelSession)
+
+possibleAmountOfPapers = np.arange(3,7)
+print(possibleAmountOfPapers)
+
+
